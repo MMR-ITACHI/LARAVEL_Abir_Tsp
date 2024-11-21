88de22dc227b2db2794782a83c11005079c6400f
MMR-Aabir <mostafizurrahmanabir4444@gmail.com>
2024-11-21 23:58:49 +0600

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        // \App\Models\Admin::create([
+        //     'name' => 'M.M.R Aabir',
+        //     'email' => 'admin@gmail.com',
+        //     'password' =>Hash::make('admin123')
+        // ]);
+
+        \App\Models\Manager::create([
+            'name' => 'M.M.R Shisui',
+            'email' => 'manager@gmail.com',
+            'password' =>Hash::make('admin123')
+        ]);
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,74 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+//Admin
+
+Route::middleware('guest:admin')->prefix('admin')->group( function () {
+
+    Route::get('login', [App\Http\Controllers\Auth\Admin\LoginController::class, 'login'])->name('admin.login');
+    Route::post('login', [App\Http\Controllers\Auth\Admin\LoginController::class, 'check_user']);
+
+    
+
+});
+
+Route::middleware('auth:admin')->prefix('admin')->group( function () {
+
+    Route::post('logout', [App\Http\Controllers\Auth\Admin\LoginController::class, 'logout'])->name('admin.logout');
+
+    Route::view('/dashboard','backend.admin_dashboard');
+
+});
+
+
+
+//Manager
+
+Route::middleware('guest:manager')->prefix('manager')->group( function () {
+
+    Route::get('login', [App\Http\Controllers\Auth\Manager\LoginController::class, 'login'])->name('manager.login');
+    Route::post('login', [App\Http\Controllers\Auth\Manager\LoginController::class, 'check_user']);
+
+    
+
+});
+
+Route::middleware('auth:manager')->prefix('manager')->group( function () {
+
+    Route::post('logout', [App\Http\Controllers\Auth\Manager\LoginController::class, 'logout'])->name('manager.logout');
+
+    Route::view('/dashboard','backend.manager1_dashboard');
+
+});
+
+require __DIR__.'/auth.php';
